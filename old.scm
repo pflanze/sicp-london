d1e28787684ce3542219401fe122753d9d0a52e6
Christian Jaeger <chrjae@gmail.com>
2013-11-28 20:31:30 +0000

    old version

---
+++ old.scm
@@ -0,0 +1,23 @@
+(define first car)
+(define rest cdr)
+(define second cadr)
+
+;; (1)
+
+(define (pascal-row l)
+  (define (subpascal l)
+    (if (or (null? l)
+	    (null? (rest l)))
+	(list) ;; '()
+	(cons (+ (first l) (second l))
+	      (subpascal (rest l)))))
+  (append (list 1) (subpascal l) (list 1)))
+
+(define (pascal* n l)
+  (if (zero? n)
+      l
+      (pascal* (- n 1) (pascal-row l))))
+
+(define (pascal n p)
+  (list-ref (pascal* n '(1)) p))
+

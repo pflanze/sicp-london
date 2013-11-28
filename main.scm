d904563b12dfb5502cd6852c2a727ae4c34faadb
Christian Jaeger <chrjae@gmail.com>
2013-11-28 20:22:53 +0000

    without lists

--- main.scm
+++ main.scm
@@ -1,31 +1,18 @@
-(define first car)
-(define rest cdr)
-(define second cadr)
 
-;; (1)
-
-(define (pascal-row l)
-  (define (subpascal l)
-    (if (or (null? l)
-	    (null? (rest l)))
-	(list) ;; '()
-	(cons (+ (first l) (second l))
-	      (subpascal (rest l)))))
-  (append (list 1) (subpascal l) (list 1)))
-
-(define (pascal* n l)
-  (if (zero? n)
-      l
-      (pascal* (- n 1) (pascal-row l))))
-
+;; (        (1)  row 0
+;;         (1 1) row 1
+;;        (1 2 1) row 2
+;;       (1 3 3 1) row 3
+;;     (1 4 6 4 1) row 4
+;;    (1 5 10 10 5 1) row 5
+;;   (1 6 15 20 15 6 1)
+;;      1 2
+;;  (1 7 21 35 35 21 7 1))
+;;       2
 (define (pascal n p)
-  (list-ref (pascal* n '(1)) p))
-
-
-;; (1 1)
-
-;; (1 2 1)
-
-;; (1 3 3 1)
-
-;;   4  
+  (if (zero? p)
+      1
+      (if (zero? n)
+	  0
+	  (+ (pascal (- n 1) (- p 1))
+	     (pascal (- n 1) p)))))

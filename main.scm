982dee60b3ac2336e9aec908bff21a72b2f3bd9e
Christian Jaeger <chrjae@gmail.com>
2013-11-28 21:07:56 +0000

    what he had
    M 2013-11-28 21:04 main.scm

--- main.scm
+++ main.scm
@@ -5,20 +5,30 @@
 
 ;; 3 3  -> 27
 
-(define (exp b n)
+;; (define (exp b n)
   
-  (define (exp-help x nmults n)
-    (cond ((= n 0)
-	   x)
+;;   (define (exp-help x nmults n)
+;;     (cond ((= n 0)
+;; 	   x)
 
-	  ((= n 1)
-	   (* x b))
+;; 	  ((= n 1)
+;; 	   (* x b))
 
-	  ((> (* nmults 2) n)
-	   (exp-help x 1 (- n nmults)))
+;; 	  ((> (* nmults 2) n)
+;; 	   (exp-help x 1 (- n nmults)))
 
-	  (else
-	   (exp-help (square x) (* nmults 2) n))))
+;; 	  (else
+;; 	   (exp-help (square x) (* nmults 2) n))))
 
-  (exp-help b 1 n))
+;;   (exp-help b 1 n))
+
+
+;; (define (exp a pow)
+;;   (if (= pow 1)
+;;       a
+;;       (exp (* a a) ())))
+
+(define (exp a b pow)
+  (if (= pow 1) a
+      (exp (* a b) b (- pow 1))))
 

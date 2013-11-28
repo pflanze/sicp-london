6b8a6a05e5e19a2c6ccf216b308250856a6e6b6c
Christian Jaeger <chrjae@gmail.com>
2013-11-28 20:11:02 +0000

    works, but with lists

--- main.scm
+++ main.scm
@@ -1,1 +1,31 @@
+(define first car)
+(define rest cdr)
+(define second cadr)
 
+;; (1)
+
+(define (pascal-row l)
+  (define (subpascal l)
+    (if (or (null? l)
+	    (null? (rest l)))
+	(list) ;; '()
+	(cons (+ (first l) (second l))
+	      (subpascal (rest l)))))
+  (append (list 1) (subpascal l) (list 1)))
+
+(define (pascal* n l)
+  (if (zero? n)
+      l
+      (pascal* (- n 1) (pascal-row l))))
+
+(define (pascal n p)
+  (list-ref (pascal* n '(1)) p))
+
+
+;; (1 1)
+
+;; (1 2 1)
+
+;; (1 3 3 1)
+
+;;   4  

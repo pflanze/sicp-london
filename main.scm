5c70eabde5dcc8282af6faacb91390f1e089c6c0
Christian Jaeger <chrjae@gmail.com>
2014-01-23 20:12:12 +0000

    (commit)
    M 2013-12-19 20:50 main.scm

--- main.scm
+++ main.scm
@@ -9,9 +9,26 @@
 	  (f (+ x dx)))
        3)))
 
+
+(define f square)
+
 (define g (smooth square))
 
 ;; repeatedly  (couple steps; heh~, vs different dx? wl digital, wlnow?)
 
 (define h (smooth g))
+
+;; hm n times?
+
+;; hm efficiently? compose ?
+
+;;(define ())
+
+;;
+
+;; (compose smooth f)
+;; (smooth f)
+
+
+
 

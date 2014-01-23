1a3d61c74b831868c25b16f32f52db924aa9aa18
Christian Jaeger <chrjae@gmail.com>
2014-01-23 21:00:18 +0000

    .
    M 2014-01-23 20:50 main.scm

--- main.scm
+++ main.scm
@@ -5,6 +5,22 @@
 	       (cons (car xs) (filter fn (cdr xs)))
 	       (filter fn (cdr xs))))))
 
+
+;; (define (odd? x)
+;;   (if (= x 1) #t
+;;       (even? (- x 1))))
+;; (define (even? x)
+;;   (if (= x 0) #t
+;;       (odd? (- x 1))))
+
+(define (even? x)
+  (integer? (/ x 2)))
+
+(define (odd? x)
+  (not (even? x)))
+
 (define (same-parity x . xs)
-  (filter (if (even? x) even? odd?)
+  (filter (if (even? x)
+	      even?
+	      odd?)
 	  xs))

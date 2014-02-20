e6d5a5f4bac39cce82d1785e386ece5ad1defe83
Christian Jaeger <ch@christianjaeger.ch>
2014-02-20 20:25:57 +0000

    me  unfertig

--- main.scm
+++ main.scm
@@ -25,3 +25,66 @@
 		   (cc (* n x)))
 		 (- n 1))))
 
+
+(define sc #f)
+(define (save! c)
+  (call/cc (lambda (cc)
+	     (set! sc cc)
+	     x)))
+
+
+(define (current-continuation)
+  (call/cc (L (cc) cc)))
+
+(define (tt)
+  (let ((cc (current-continuation)))
+    ;; (cc cc)  dann
+    (cc 0)))
+
+
+
+;; label goto
+
+
+(define label current-continuation)
+
+(define (goto l)
+  (l l))
+
+(define (t3)
+  (define c 0)
+  (let ((l (label)))
+    (println c)
+    (set! c (+ c 1))
+    (if (< c 10)
+	(goto l))))
+
+
+;; (define (t3)
+;;   (define c 0)
+;;   (lambda (XXX)
+;;     (let ((l XXX))
+;;       (println c)
+;;       (set! c (+ c 1))
+;;       (if (< c 10)
+;; 	  (goto l)))))
+
+
+(define (t4 yield)
+  (define c 0)
+  (let ((l (label)))
+    (yield c)
+    (set! c (+ c 1))
+    (if (< c 10)
+	(goto l))))
+
+(define (use-t4.1)
+  (t4 println))
+
+;; (define (produce fn)
+;;   (call/cc (L (c)
+;; 	      )))
+
+(define (use-t4.1)
+  (produce t4))
+

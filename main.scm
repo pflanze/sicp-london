c9a135f350f33efe02d9e10adfe5b9b0cb1c769e
Christian Jaeger <ch@christianjaeger.ch>
2014-02-20 19:27:47 +0000

    von ihm

--- main.scm
+++ main.scm
@@ -8,3 +8,20 @@
   (id main 1))
 
 
+;; (define (factorial cc n)
+;;   (if (= n 0) ;;wl
+;;       (cc 1)
+;;       ;; fact (n+1) + fact (n+2)  gl
+;;       (factorial (lambda (cc n)
+;; 		   (factorial cc (- n 2)))
+;; 		 (- n 1))))
+
+;;wrong fib depp
+
+(define (factorial cc n)
+  (if (= n 0)
+      (cc 1)
+      (factorial (lambda (x)
+		   (cc (* n x)))
+		 (- n 1))))
+

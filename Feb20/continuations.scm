23ea0718db0238e4a0ebf9d728afa47f58682185
Christian Jaeger <ch@christianjaeger.ch>
2014-02-27 15:59:51 +0000

    cleanup

--- Feb20/continuations.scm
+++ Feb20/continuations.scm
@@ -8,16 +8,6 @@
   (id main 1))
 
 
-;; (define (factorial cc n)
-;;   (if (= n 0) ;;wl
-;;       (cc 1)
-;;       ;; fact (n+1) + fact (n+2)  gl
-;;       (factorial (lambda (cc n)
-;; 		   (factorial cc (- n 2)))
-;; 		 (- n 1))))
-
-;;wrong fib depp
-
 (define (factorial cc n)
   (if (= n 0)
       (cc 1)
@@ -38,13 +28,12 @@
 
 (define (tt)
   (let ((cc (current-continuation)))
-    ;; (cc cc)  dann
+    ;; (cc cc)  endless loop; then: (0 not a procedure)
     (cc 0)))
 
 
 
 ;; label goto
-
 
 (define label current-continuation)
 

37708f066d5ee6209379f332f07fe34f8f63c38d
Christian Jaeger <ch@christianjaeger.ch>
2014-03-06 21:04:10 +0000

    (well. not close.  how does the tree work? actual reading would help..)

--- main.scm
+++ main.scm
@@ -50,6 +50,11 @@
                    (make-code-tree (make-leaf 'D 1)
                                    (make-leaf 'C 1)))))
 
+
+(define (search-tree t sym)
+  )
+
+
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
 (TEST
@@ -57,12 +62,22 @@
  (A D A B B C A)
  )
 
-(define test-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(define test-message (decode sample-message sample-tree))
 
 (define (encode message tree)
-  (cond ((null? message)
-	 '())
-	(else
-	 (let ((c (car message)))
-	   ))))
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+;; Encode-symbol is a procedure, which you must write, that returns
+;; the list of bits that encodes a given symbol according to a given
+;; tree. You should design encode-symbol so that it signals an error
+;; if the symbol is not in the tree at all. Test your procedure by
+;; encoding the result you obtained in exercise 2.67 with the sample
+;; tree and seeing whether it is the same as the original sample
+;; message.
+
+(define (encode-symbol c tree)
+  ç)
 

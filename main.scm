d666885728a02d78405ba831f398288e8f612e7f
Christian Jaeger <ch@christianjaeger.ch>
2014-03-13 20:01:12 +0000

    sigh. what where

--- main.scm
+++ main.scm
@@ -17,6 +17,21 @@
       (weight-leaf tree)
       (cadddr tree)))
 
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((= x (entry set)) true)
+;;         ((< x (entry set))
+;;          (element-of-set? x (left-branch set)))
+;;         ((> x (entry set))
+;;          (element-of-set? x (right-branch set)))))
+
 
 
 (define (adjoin-set x set)
@@ -24,6 +39,11 @@
         ((< (weight x) (weight (car set))) (cons x set))
         (else (cons (car set)
                     (adjoin-set x (cdr set))))))
+
+(TEST
+ > (adjoin-set '(leaf F 1) (adjoin-set '(leaf A 1) '()))
+ ((leaf A 1) (leaf F 1)))
+
 
 
 
@@ -84,5 +104,5 @@
 ;; root of the entire tree.
 ;; ((what about 2?--well merge?))
 
-(define (successive-merge s)
+'(define (successive-merge s)
   )

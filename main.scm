b5da762e7dd7de7ec51a2591d006b4d8f7c48da7
Christian Jaeger <ch@christianjaeger.ch>
2014-03-13 19:52:29 +0000

    . ?

--- main.scm
+++ main.scm
@@ -26,6 +26,12 @@
                     (adjoin-set x (cdr set))))))
 
 
+
+;; The following procedure takes a list of symbol-frequency pairs such
+;; as ((A 4) (B 2) (C 1) (D 1)) and constructs an initial ordered set
+;; of leaves, ready to be merged according to the Huffman algorithm:
+
+
 (define (make-leaf-set pairs)
   (if (null? pairs)
       '()
@@ -62,3 +68,21 @@
 	  (F 1))
 	(on cadr >)))
 
+
+;; Huffman tree:
+
+;; has   binary  left right  on  sets  if part of set? . but what is weight for?
+;;???
+
+;; merge 2 of same
+
+;; At each step, merge two nodes with the smallest weights, removing
+;; them from the set and replacing them with a node that has these two
+;; as its left and right branches.
+
+;; The process stops when there is only one node left, which is the
+;; root of the entire tree.
+;; ((what about 2?--well merge?))
+
+(define (successive-merge s)
+  )

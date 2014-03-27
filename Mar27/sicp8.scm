a0e7d1a3967ff20760c728b123559f5fbf4e7522
Christian Jaeger <ch@christianjaeger.ch>
2014-03-27 02:18:38 +0000

    remove duplicate code (that removes the =zero? etc., and/or misses type bug fix)

--- Mar27/sicp8.scm
+++ Mar27/sicp8.scm
@@ -274,21 +274,6 @@
 (define (make-rational n d)
   ((get 'make 'rational) n d))
 
-(define (install-scheme-number-package)
-  (define (tag x)
-    (attach-tag 'scheme-number x))    
-  (put 'add '(scheme-number scheme-number)
-       (lambda (x y) (tag (+ x y))))
-  (put 'sub '(scheme-number scheme-number)
-       (lambda (x y) (tag (- x y))))
-  (put 'mul '(scheme-number scheme-number)
-       (lambda (x y) (tag (* x y))))
-  (put 'div '(scheme-number scheme-number)
-       (lambda (x y) (tag (/ x y))))
-  (put 'make 'scheme-number
-       (lambda (x) (tag x)))
-  'done)
-
 (define (make-scheme-number n)
   ((get 'make 'scheme-number) n))
 
